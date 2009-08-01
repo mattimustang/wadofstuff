6d4932d0fb149fc20ed808d9be0cb97513282e3d
mattimustang <mattimustang@a70a79b1-732b-0410-87e2-d97bea89e118>
2009-08-01 04:53:46 +0000

    Much improved version with:
     * command line arguments for svn log uri, title and Atom feed link.
     * better error handling.
     * SvnLogHandler no longer prints to stdout be stores everything in 
     `content` property.
     * generate() function which can be imported by other python scripts.

--- python/misc/svnlogatom.py
+++ python/misc/svnlogatom.py
@@ -2,76 +2,131 @@
 """Convert output from svn log -v --xml to ATOM"""
 
 from datetime import datetime
+import sys
 import urllib2
-from xml.sax import parse
+from xml.sax import make_parser, SAXParseException
 from xml.sax.handler import ContentHandler 
 from xml.sax.saxutils import escape
 
-URL='http://extjs.com/products/extjs/svnlog.xml'
+__version__ = '1.0'
 
-class ExtHandler(ContentHandler):
-
+class SvnLogHandler(ContentHandler):
+    """Convert Subversion Commit Log XML to Atom."""
     def __init__(self):
+        """Initialise instance variables."""
+        self._content = ''
         self.paths = []
         self.msg = ''
-        self.isMsg = False
-        self.isAuthor = False
-        self.isDate = False
-        self.isPath = False
+        self.author = ''
+        self.date = ''
+        self.revision = ''
+        self.is_msg = False
+        self.is_author = False
+        self.is_date = False
+        self.is_path = False
+        ContentHandler.__init__(self)
+
+    @property
+    def content(self):
+        """The generated Atom content."""
+        return self._content
 
     def startElement(self, name, attrs):
+        """Set flags depending on which Subversion log element is parsed."""
         if name == 'logentry':
             self.revision = attrs.get('revision', "")
         elif name == 'msg':
-            self.isMsg = True
+            self.is_msg = True
         elif name == 'author':
-            self.isAuthor = True
+            self.is_author = True
         elif name == 'date':
-            self.isDate = True
+            self.is_date = True
         elif name == 'path':
-            self.isPath = True
+            self.is_path = True
             self.paths.append([attrs.get('action', "")])
         return
             
     def characters(self, content):
-        if self.isMsg:
+        """Extract the content of the element."""
+        if self.is_msg:
             self.msg += content
-        elif self.isAuthor:
+        elif self.is_author:
             self.author = content
-        elif self.isDate:
+        elif self.is_date:
             self.date = content
-        elif self.isPath:
+        elif self.is_path:
             self.paths[-1].append(content)
 
     def endElement(self, name):
+        """Reset all the element flags and generate Atom XML if we have
+        reached the end of a logentry."""
         if name == 'logentry':
-            print "<entry>"
-            print '<title type="text">%s</title>' % escape("%s: %s" % (self.revision, self.msg.replace('\n', ' ')))
-            print "<id>%s</id>" % self.revision
-            print "<updated>%s</updated>" % self.date
-            print "<author><name>%s</name></author>" % self.author
-            print '<summary type="html" xml:space="preserve">%s</summary>' % escape('<p>%s</p><p>Files:<ul>%s</ul></p>' % (self.msg.replace('\n', '<br/>'), ''.join(["<li>%s %s</li>" % (a,p) for a,p in self.paths])))
-            print "</entry>"
+            self._content += "<entry>"
+            _title = escape("%s: %s" % (self.revision,
+                self.msg.replace('\n', ' ')))
+            self._content += '<title type="text">%s</title>' % _title
+            self._content += "<id>%s</id>" % self.revision
+            self._content += "<updated>%s</updated>" % self.date
+            self._content += "<author><name>%s</name></author>" % self.author
+            _summary = escape('<p>%s</p><p>Files:<ul>%s</ul></p>' % 
+                (self.msg.replace('\n', '<br/>'),
+                ''.join(["<li>%s %s</li>" % (a,p) for a,p in self.paths])))
+            self._content += ('<summary type="html" xml:space="preserve">%s'
+                '</summary>' % _summary)
+            self._content += "</entry>"
             self.paths = []
             self.msg = ''
         elif name == 'msg':
-            self.isMsg = False
+            self.is_msg = False
         elif name == 'author':
-            self.isAuthor = False
+            self.is_author = False
         elif name == 'date':
-            self.isDate = False
+            self.is_date = False
         elif name == 'path':
-            self.isPath = False
+            self.is_path = False
 
-if __name__ == '__main__':
+def generate(file_object, title, link):
+    """Generate the Atom xml."""
     now = datetime.utcnow()
     now = "%sT%s:%s:%sZ" % (now.date(), now.hour, now.minute, now.second)
-    print """<?xml version="1.0" encoding="utf-8"?>
+    header = """<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-<title>EXTJS SVN Commit Log</title>
-<link href="http://extjs.com/products/extjs/commitlog.php" />
+<title>%s</title>
+<link href="%s" />
 <updated>%s</updated>
-<generator uri="svnlogatom.py" version="1.0">SVN Log to Atom</generator>
-""" % now
-    parse(urllib2.urlopen(URL), ExtHandler())
-    print "</feed>"
+<generator uri="svnlogatom.py" version="%s">SVN Log to Atom</generator>
+""" % (title, link, now, __version__)
+    footer = "</feed>"
+    parser = make_parser()
+    handler = SvnLogHandler()
+    parser.setContentHandler(handler)
+    try:
+        parser.parse(file_object)
+    except SAXParseException, err:
+        sys.stderr.write("%s\n" % err)
+        sys.exit(1)
+    content = parser.getContentHandler().content
+    return "%s%s%s" % (header, content, footer)
+
+if __name__ == '__main__':
+    __title = ''
+    __link = ''
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: %s uri [title] [link]\n" % sys.argv[0])
+        sys.exit(2)
+
+    try:
+        __file_obj = urllib2.urlopen(sys.argv[1])
+    except Exception, __err:
+        sys.stderr.write("%s\n" % __err)
+        sys.exit(2)
+
+    try:
+        __title = sys.argv[2]
+    except IndexError:
+        __title = sys.argv[1]
+    try:
+        __link = sys.argv[3]
+    except IndexError:
+        __link = sys.argv[1]
+    print generate(__file_obj, __title, __link)
