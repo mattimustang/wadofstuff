be2976e66a12ae1668007813c2bfe7c4b375aa89
mattimustang <mattimustang@a70a79b1-732b-0410-87e2-d97bea89e118>
2010-03-21 09:54:55 +0000

    Most string converted to unicode and output encoded as utf8. Made ATOM output validate by adding proper id tags to feed and entries, a link rel=alternate to items. Fixed formatting of updated date string.

--- python/misc/svnlogatom.py
+++ python/misc/svnlogatom.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python2.4
 """Convert output from svn log -v --xml to ATOM"""
 
 from datetime import datetime
+import re
 import sys
 import urllib2
 from xml.sax import make_parser, SAXParseException
 from xml.sax.handler import ContentHandler 
 from xml.sax.saxutils import escape
+ATOM_ID = ''
 
 __version__ = '1.0'
 
@@ -14,12 +16,12 @@ class SvnLogHandler(ContentHandler):
     """Convert Subversion Commit Log XML to Atom."""
     def __init__(self):
         """Initialise instance variables."""
-        self._content = ''
+        self._content = u''
         self.paths = []
-        self.msg = ''
-        self.author = ''
-        self.date = ''
-        self.revision = ''
+        self.msg = u''
+        self.author = u''
+        self.date = u''
+        self.revision = u''
         self.is_msg = False
         self.is_author = False
         self.is_date = False
@@ -34,7 +36,7 @@ class SvnLogHandler(ContentHandler):
     def startElement(self, name, attrs):
         """Set flags depending on which Subversion log element is parsed."""
         if name == 'logentry':
-            self.revision = attrs.get('revision', "")
+            self.revision = attrs.get('revision', u"")
         elif name == 'msg':
             self.is_msg = True
         elif name == 'author':
@@ -43,7 +45,7 @@ class SvnLogHandler(ContentHandler):
             self.is_date = True
         elif name == 'path':
             self.is_path = True
-            self.paths.append([attrs.get('action', "")])
+            self.paths.append([attrs.get('action', u""),u''])
         return
             
     def characters(self, content):
@@ -55,27 +57,28 @@ class SvnLogHandler(ContentHandler):
         elif self.is_date:
             self.date = content
         elif self.is_path:
-            self.paths[-1].append(content)
+            self.paths[-1][1] += content
 
     def endElement(self, name):
         """Reset all the element flags and generate Atom XML if we have
         reached the end of a logentry."""
         if name == 'logentry':
             self._content += "<entry>"
-            _title = escape("%s: %s" % (self.revision,
+            _title = escape(u"%s: %s" % (self.revision,
                 self.msg.replace('\n', ' ')))
-            self._content += '<title type="text">%s</title>' % _title
-            self._content += "<id>%s</id>" % self.revision
-            self._content += "<updated>%s</updated>" % self.date
-            self._content += "<author><name>%s</name></author>" % self.author
-            _summary = escape('<p>%s</p><p>Files:<ul>%s</ul></p>' % 
+            self._content += u'<title type="text">%s</title>' % _title
+            self._content += u"<id>%s:%s</id>" % (ATOM_ID, self.revision)
+            self._content += u"<updated>%s</updated>" % self.date
+            self._content += u'<link rel="alternate" href="#" />'
+            self._content += u"<author><name>%s</name></author>" % self.author
+            _summary = escape(u'<p>%s</p><p>Files:<ul>%s</ul></p>' % 
                 (self.msg.replace('\n', '<br/>'),
-                ''.join(["<li>%s %s</li>" % (a,p) for a,p in self.paths])))
-            self._content += ('<summary type="html" xml:space="preserve">%s'
+                ''.join([u"<li>%s %s</li>" % (a,p) for a,p in self.paths])))
+            self._content += (u'<summary type="html" xml:space="preserve">%s'
                 '</summary>' % _summary)
-            self._content += "</entry>"
+            self._content += u"</entry>"
             self.paths = []
-            self.msg = ''
+            self.msg = u''
         elif name == 'msg':
             self.is_msg = False
         elif name == 'author':
@@ -87,15 +90,18 @@ class SvnLogHandler(ContentHandler):
 
 def generate(file_object, title, link):
     """Generate the Atom xml."""
+    global ATOM_ID 
     now = datetime.utcnow()
-    now = "%sT%s:%s:%sZ" % (now.date(), now.hour, now.minute, now.second)
-    header = """<?xml version="1.0" encoding="utf-8"?>
+    now = u"%sT%.2d:%.2d:%.2dZ" % (now.date(), now.hour, now.minute, now.second)
+    ATOM_ID = u'tag:svnlogatom.py,2010:%s' % re.sub(r'^.*:/+', '', file_object.url)
+    header = u"""<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
 <title>%s</title>
 <link href="%s" />
 <updated>%s</updated>
+<id>%s</id>
 <generator uri="svnlogatom.py" version="%s">SVN Log to Atom</generator>
-""" % (title, link, now, __version__)
+""" % (title, link, now, ATOM_ID, __version__)
     footer = "</feed>"
     parser = make_parser()
     handler = SvnLogHandler()
@@ -106,7 +112,7 @@ def generate(file_object, title, link):
         sys.stderr.write("%s\n" % err)
         sys.exit(1)
     content = parser.getContentHandler().content
-    return "%s%s%s" % (header, content, footer)
+    return u"%s%s%s" % (header, content, footer)
 
 if __name__ == '__main__':
     __title = ''
@@ -129,4 +135,4 @@ if __name__ == '__main__':
         __link = sys.argv[3]
     except IndexError:
         __link = sys.argv[1]
-    print generate(__file_obj, __title, __link)
+    print generate(__file_obj, __title, __link).encode('utf8')
